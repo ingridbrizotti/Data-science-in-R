f461e41ba412729fe6109e9b347b8ba13399dc36
Ingrid Brizotti <ingridbrizotti@gmail.com>
2017-02-04 18:58:14 -0800

    Add files via upload

---
+++ basic packages.R
@@ -0,0 +1,144 @@
+
+
+################################### LOAD DATA ############################################################
+
+# If you'd like to read in data from a database
+install.packages("RMySQL")
+
+# to read and write Micorsoft Excel files from R
+install.packages("XLConnect")
+install.packages("xlsx")
+
+# to read SAS or SPSS
+install.packages("foreign")
+
+ 
+
+################################### MANAGE DATA ############################################################
+
+# Essential shortcuts for subsetting, summarizing, rearranging, and joining together data sets. 
+# dplyr is our go to package for fast data manipulation.
+install.packages("dplyr")
+
+# Tools for changing the layout of your data sets.
+install.packages("tidyr")
+
+# regular expressions and character strings
+install.packages("stringr")
+
+# Tools that make working with dates and times easier
+install.packages("lubridate") 
+
+
+
+################################### VISUALIZE DATA ############################################################
+
+
+# beautiful graphics :)
+install.packages("ggplot2") 
+ 
+# 3D visualizations 
+install.packages("rgl")
+
+# use Google Chart tools to visualize data in R (Gapminder)
+install.packages("googleVis")
+
+# maps
+install.packages("leafleteVis")
+
+# time series
+install.packages("dygraphs")
+
+# tables
+install.packages("dt")
+
+# diagrams
+install.packages("diagrammeR")
+
+
+################################### MODEL DATA ############################################################
+
+# Generalized Additive Models
+install.packages("mgcv")
+
+# Linear mixed effects models
+install.packages("lme4")
+
+# Non-Linear mixed effects models
+install.packages("nlme")
+
+# random forest
+install.packages("randomForest")
+
+# multiple comparison testing
+install.packages("multcomp")
+
+# Visualization tools and tests for categorical data
+install.packages("vcd")
+
+# Lasso and elastic-net regression methods with cross validation
+install.packages("glmnet")
+
+# survival analysis
+install.packages("survival")
+
+# training regression and classification models
+install.packages("caret")
+install.packages("gmodels")
+
+# decision tree
+install.packages("rpart")
+
+# ROC curve
+install.packages("ROCR")
+
+
+################################### REPORT RESULTS ############################################################
+
+
+install.packages("shiny")
+
+# Reporting
+install.packages("R Markdown")
+
+
+################################### SPATIAL DATA ############################################################
+
+# loading and using spatial data including shapefiles
+install.packages("sp")
+install.packages("maptools")
+
+# use map polygons for plots
+install.packages("maps")
+
+# Download street maps straight from Google maps and use them as a background in your ggplots.
+install.packages("ggmap")
+
+
+######################### TIME SERIES AND FINANCIAL DATA ############################################################
+
+
+# format for saving time series objects in R
+install.packages("zoo")
+
+# manipulating time series data sets
+install.packages("xts")
+
+# Tools for downloading financial data, plotting common charts, and doing technical analysis
+install.packages("quantmod")
+
+# To write high performance R code
+install.packages("quantmod")
+
+# Big Data (An alternative way to organize data sets for very, very fast operations)
+install.packages("data.table")
+
+
+# Use parallel processing in R to speed up your code or to crunch large data sets
+install.packages("parallel")
+
+
+
+
+
+

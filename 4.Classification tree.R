ddd2dd35d056381d1be3109c3edd2e3eb998473b
Ingrid Brizotti <ingridbrizotti@gmail.com>
2017-06-16 13:56:19 -0400

    Update 4.Classification tree.R

--- 4.Classification tree.R
+++ 4.Classification tree.R
@@ -26,3 +26,29 @@ tree1 <- rpart(formula = left ~  satisfaction_level
 # plot tree
 plot(tree1, uniform=TRUE, main="Classification Tree")
 text(tree1, use.n=TRUE, all=TRUE, cex=.8)
+
+# confusion matrix (training data)
+conf_matrix_tree <- table(train$left, predict(tree1, type="class"))
+rownames(conf_matrix_tree) <- paste("Actual", rownames(conf_matrix_tree), sep = ":")
+colnames(conf_matrix_tree) <- paste("Pred", colnames(conf_matrix_tree), sep = ":")
+print(conf_matrix_tree)
+
+# On test set
+test_tree = predict(tree1, test, type = "prob")
+
+#Storing Model Performance Scores
+pred_tree <-prediction(test_tree[,2], test$left)
+
+# Calculating Area under Curve
+perf_tree <- performance(pred_tree,"auc")
+perf_tree
+
+# Calculating True Positive and False Positive Rate
+perf_tree <- performance(pred_tree, "tpr", "fpr")
+
+# Plot the ROC curve
+plot(perf_tree,  lwd = 1.5)
+
+#Calculating KS statistics
+ks1.tree <- max(attr(perf_tree, "y.values")[[1]] - (attr(perf_tree, "x.values")[[1]]))
+ks1.tree
